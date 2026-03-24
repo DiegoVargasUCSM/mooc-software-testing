f8df49e7a08d9a31654bece414a8db9036beac4e
DiegoVargasUCSM <diego.vargasv@ucsm.edu.pe>
2026-03-24 15:46:57 -0500

    Agregamos pruebas de funciones en base a las modificanes de la clase GHAPPY

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,43 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    @Test
+    public void happyGsTogether() {
+        GHappy gh = new GHappy();
+        Assertions.assertTrue(gh.gHappy("xxggxx"));
+    }
+
+    @Test
+    public void singleGNotHappy() {
+        GHappy gh = new GHappy();
+        Assertions.assertFalse(gh.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void mixedGsNotAllHappy() {
+        GHappy gh = new GHappy();
+        Assertions.assertFalse(gh.gHappy("xxggyygxx"));
+    }
+
+    @Test
+    public void noGs() {
+        GHappy gh = new GHappy();
+        Assertions.assertTrue(gh.gHappy("abcdef"));
+    }
+
+    @Test
+    public void gAtBeginning_bugCase() {
+        GHappy gh = new GHappy();
+        Assertions.assertFalse(gh.gHappy("gxx"));
+    }
+
+    @Test
+    public void gAtEnd() {
+        GHappy gh = new GHappy();
+        Assertions.assertFalse(gh.gHappy("xxg"));
+    }
 }
