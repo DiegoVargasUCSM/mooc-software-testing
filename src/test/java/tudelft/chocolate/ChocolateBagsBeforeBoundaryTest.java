c9811643af7c52b37ec6bca36f5d7434015f4c0e
DiegoVargasUCSM <diego.vargasv@ucsm.edu.pe>
2026-03-24 14:51:05 -0500

    Modificacion de la clase CHOCOLATEBAGSTEST ya que le agregamos 2 funciones mas

--- src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ChocolateBagsBeforeBoundaryTest {
+
     @Test
     public void totalIsBiggerThanAmountOfBars() {
         ChocolateBags bags = new ChocolateBags();
@@ -27,5 +28,29 @@ public class ChocolateBagsBeforeBoundaryTest {
     public void onlySmallBars() {
         int result = new ChocolateBags().calculate(4, 2, 3);
         Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    public void boundary_totalJustEnoughBars() {
+        int result = new ChocolateBags().calculate(1, 1, 6);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    public void boundary_totalJustAboveLimit() {
+        int result = new ChocolateBags().calculate(1, 1, 7);
+        Assertions.assertEquals(-1, result);
+    }
+
+    @Test
+    public void boundary_needSmallBars() {
+        int result = new ChocolateBags().calculate(5, 2, 11);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    public void boundary_onlySmallLimit() {
+        int result = new ChocolateBags().calculate(2, 0, 3);
+        Assertions.assertEquals(-1, result);
     }
 }
