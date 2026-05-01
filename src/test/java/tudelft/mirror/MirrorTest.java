1c0e7ba14eceb7f978220b5d1eeb24bf0dc58862
DiegoVargasUCSM <diego.vargasv@ucsm.edu.pe>
2026-04-30 23:05:42 -0500

    Creamos la clase MIRRORTEST

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,43 @@
 package tudelft.mirror;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    void testExampleOne() {
+        Mirror mirror = new Mirror();
+
+        assertEquals("ab", mirror.mirrorEnds("abXYZba"));
+    }
+
+    @Test
+    void testExampleTwo() {
+        Mirror mirror = new Mirror();
+
+        assertEquals("a", mirror.mirrorEnds("abca"));
+    }
+
+    @Test
+    void testOddLengthPalindrome() {
+        Mirror mirror = new Mirror();
+
+        assertEquals("aba", mirror.mirrorEnds("aba"));
+    }
+
+    @Test
+    void testEvenLengthMirror() {
+        Mirror mirror = new Mirror();
+
+        assertEquals("ab", mirror.mirrorEnds("abba"));
+    }
+
+    @Test
+    void testNoMirror() {
+        Mirror mirror = new Mirror();
+
+        assertEquals("", mirror.mirrorEnds("abc"));
+    }
 }
