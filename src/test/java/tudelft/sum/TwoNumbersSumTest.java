3a05553aca88b2221bae503bc5f66201496e36a5
DiegoVargasUCSM <diego.vargasv@ucsm.edu.pe>
2026-04-30 22:50:37 -0500

    Completamos la clase TWONUMBERSSUMTEST

--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,59 @@
 package tudelft.sum;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
 public class TwoNumbersSumTest {
 
+    @Test
+    void testSimpleSum() {
+        TwoNumbersSum sum = new TwoNumbersSum();
+
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(1,2));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(3,4));
+
+        ArrayList<Integer> result = sum.addTwoNumbers(first, second);
+
+        assertEquals(Arrays.asList(4,6), result);
+    }
+
+    @Test
+    void testCarrySum() {
+        TwoNumbersSum sum = new TwoNumbersSum();
+
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(9));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(1));
+
+        ArrayList<Integer> result = sum.addTwoNumbers(first, second);
+
+        assertEquals(Arrays.asList(1,0), result);
+    }
+
+    @Test
+    void testDifferentSizes() {
+        TwoNumbersSum sum = new TwoNumbersSum();
+
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(1,2,3));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(4,5));
+
+        ArrayList<Integer> result = sum.addTwoNumbers(first, second);
+
+        assertEquals(Arrays.asList(1,6,8), result);
+    }
+
+    @Test
+    void testFinalCarryBug() {
+        TwoNumbersSum sum = new TwoNumbersSum();
+
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(9,9));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(1));
+
+        ArrayList<Integer> result = sum.addTwoNumbers(first, second);
+
+        assertEquals(Arrays.asList(1,0,0), result);
+    }
 }
